a2af2526fc9c0e4a25f515a1770fe83f457ecadc
rodrigoparaboamartins <rodrigoparaboa@hotmail.com>
2023-09-26 20:21:45 +1000

    complete lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -10,8 +10,7 @@ describe("what-is-a-test", () => {
   });
 
   describe("Height", () => {
-    it("is less than 40 and greater than 0", () => {
-      expect(height).toBeMoreThan(0)
+    it("is less than 40", () => {
       expect(height).toBeLessThan(40);
     });
   });
